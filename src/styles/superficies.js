a3fd8596b543a26bd91cf73e2d3c95ec44f6ccef
Bruno Cunha <bruno.silva.cunha@hotmail.com>
2020-06-04 20:56:59 -0300

    Created card container

--- src/styles/superficies.js
+++ src/styles/superficies.js
@@ -2,9 +2,7 @@ import styles from 'styled-components';
 
 export const Card = styles.div`
     background-color: #fff;
-    padding: 1rem;
     border-radius: 5px;
-    box-shadow: 0px 2px 1px -1px rgba(0,0,0,0.2), 
-                0px 1px 1px 0px rgba(0,0,0,0.14), 
-                0px 1px 3px 0px rgba(0,0,0,0.12);
+    background: #FFFFFF;
+    box-shadow: 0px 6px 16px rgba(0, 0, 0, 0.05);
 `;
